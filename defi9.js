05b66fc4ef8de851597daba5e209274facb75a46
aymen ELFADILI <aitabbou111@gmail.com>
2025-10-30 10:43:13 +0100

    Problem Solving to defi 9

--- defi9.js
+++ defi9.js
@@ -3,11 +3,11 @@ let echange = 3;
 
 
 if(boit % echange == 0){
-    let res = boit / echange + 1;
+    let res = boit + boit / echange + 1;
     console.log(res)
 }else{
     let d = boit/echange;
-    let res = Math.trunc(d) + 1;
+    let res = boit + Math.trunc(d) + 1;
 
     console.log(res);
 }
